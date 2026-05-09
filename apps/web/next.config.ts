30b7b78ca62305c1a18f47a5a978f521aeb350c8
oleg.cherevko <cherevko.mail@gmail.com>
2026-05-09 11:56:57 +0300

    fix: enhance output file tracing configuration in next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 import nextPWA from "next-pwa";
+import path from "node:path";
 
 const withPWA = nextPWA({
   dest: "public",
@@ -11,6 +12,15 @@ const withPWA = nextPWA({
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@bonus-tracker/ui", "@bonus-tracker/db"],
+  outputFileTracingRoot: path.join(process.cwd(), "../../"),
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/.prisma/client/**/*",
+      "../../node_modules/.pnpm/@prisma+client*/node_modules/.prisma/client/**/*",
+      "../../node_modules/.pnpm/@prisma+client*/node_modules/@prisma/client/**/*",
+      "../../node_modules/.pnpm/@prisma+engines*/node_modules/@prisma/engines/**/*",
+    ],
+  },
 };
 
 export default withPWA(nextConfig);
